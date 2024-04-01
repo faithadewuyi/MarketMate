7037f2d70ecbb4144393a365953359024510074f
Elizabeth <betty4web@gmail.com>
2024-04-01 13:58:46 +0100

    feat:navbar and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div className="w-screen bg-terblue">
+        <div className="w-4/5 items-center mx-auto font-poppins text-white pt-6">
+          <div className="flex justify-between">
+            <div>
+              <h3 className="text-2xl">Your all in one shopping site</h3>
+              <p className="text-sm">The quick fox jumps over the lazy dog</p>
+            </div>
+            <button className="bg-primaryred text-sm font-bold py-2 px-6">
+              Contact Us
+            </button>
+          </div>
+          <div className="grid md:grid-cols-5 grid-cols-2 gap-4 py-6">
+            <ul>
+              <li className="text-base pb-1.2">Company Info</li>
+              <li className="pb-1 text-xs">
+                <a href="#">About Us</a>
+              </li>
+              <li className="pb-1 text-xs">
+                <a href="#">Carrier</a>
+              </li>
+              <li className="pb-1 text-xs">
+                <a href="#">We are hiring</a>
+              </li>
+              <li className=" text-xs">
+                <a href="#">Blog</a>
+              </li>
+            </ul>
+            <ul>
+              <li className="text-base pb-1.2">Legal</li>
+              <li className="pb-1 text-xs">
+                <a href="#">About Us</a>
+              </li>
+              <li className="pb-1 text-xs">
+                <a href="#">Carrier</a>
+              </li>
+              <li className="pb-1 text-xs">
+                <a href="#">We are hiring</a>
+              </li>
+              <li className=" text-xs">
+                <a href="#">Blog</a>
+              </li>
+            </ul>
+            <ul>
+              <li className="text-base pb-1.2">Features</li>
+              <li className="pb-1 text-xs">
+                <a href="#">Business Marketing</a>
+              </li>
+              <li className="pb-1 text-xs">
+                <a href="#">User Analytic</a>
+              </li>
+              <li className="pb-1 text-xs">
+                <a href="#">Live Chat</a>
+              </li>
+              <li className=" text-xs">
+                <a href="#">Unlimited Support</a>
+              </li>
+            </ul>
+            <ul>
+              <li className="text-base pb-1.2">Resources</li>
+              <li className="pb-1 text-xs">
+                <a href="#">IOS & Android</a>
+              </li>
+              <li className="pb-1 text-xs">
+                <a href="#">Watch a Demo</a>
+              </li>
+              <li className="pb-1 text-xs">
+                <a href="#">Customers</a>
+              </li>
+              <li className=" text-xs">
+                <a href="#">API</a>
+              </li>
+            </ul>
+            <ul>
+              <li className="text-base pb-1.2">Get In Touch</li>
+              <li className="flex pb-1 text-xs">
+                <span class="material-symbols-outlined">call</span>
+                <p>(234)800 555-0103</p>
+              </li>
+              <li className="pb-1 text-xs">
+                <a href="" className="flex">
+                  <span class="material-symbols-outlined">location_on</span>
+                  <p>314 Lekki Phase 1. Lagos, Nigeria</p>
+                </a>
+              </li>
+              <li>
+                <a href="#" className="flex text-xs">
+                  <span class="material-symbols-outlined">mail</span>
+                  <p>debra.holt@example.com</p>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className=" flex justify-between flex-col md:flex-row">
+            <p className="text-sm">
+              Made With Love By Tech Amazons All Right Reserved
+            </p>
+            <div className="flex space-x-2 pt-3 md:pt-0">
+              <i class="bx bxl-facebook-circle"></i>
+              <i class="bx bxl-instagram"></i>
+              <i class="bx bxl-twitter"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,76 @@
-import { FaPhone, FaRegEnvelope, FaInstagram, FaYoutube, FaFacebook, FaTwitter } from 'react-icons/fa6';
+import {
+  FaPhone,
+  FaRegEnvelope,
+  FaInstagram,
+  FaYoutube,
+  FaFacebook,
+  FaTwitter,
+} from "react-icons/fa6";
 
 const NavBar = () => {
   return (
-    <div>
-      <header className="hidden bg-darkred text-white w-full lg:flex  justify-between px-4 py-2 fixed top-0 left-0 z-50">
-        <div className="flex items-center space-x-4">
-          <p className="flex items-center "><FaPhone />(225) 555-0118</p>
-          <p className="flex items-center"><FaRegEnvelope />info@marketmate.com</p>
-        </div>
-        
-        <p className="flex-grow text-center">Follow Us and get a chance to win 80% off</p>
-        
-        <div className="flex items-center space-x-2">
-          <FaInstagram />
-          <FaYoutube />
-          <FaFacebook />
-          <FaTwitter />
-          <p>Follow Us</p>
+    <div className="fixed top-0 w-screen z-50">
+      <header className="hidden bg-darkred text-white w-full lg:flex  px-4 py-2 left-0  ">
+        <div className="w-5/6 flex items-center mx-auto justify-between">
+          <div className="flex items-center space-x-4">
+            <p className="flex items-center ">
+              <FaPhone />
+              (225) 555-0118
+            </p>
+            <p className="flex items-center">
+              <FaRegEnvelope />
+              info@marketmate.com
+            </p>
+          </div>
+
+          <p className="flex-grow text-center">
+            Follow Us and get a chance to win 80% off
+          </p>
+
+          <div className="flex items-center space-x-2">
+            <FaInstagram />
+            <FaYoutube />
+            <FaFacebook />
+            <FaTwitter />
+            <p>Follow Us</p>
+          </div>
         </div>
       </header>
+
+      <nav className=" bg-white mb-8 py-4  justify-center">
+        <div className="w-5/6 flex items-center mx-auto">
+          <div className="flex basis-3/12">
+            <img src="Logo.png" alt="Market mate Logo" />
+            <h1 className="font-lobstertwo text-primaryred text-4xl pl-2s">
+              MarketMate
+            </h1>
+          </div>
+          <div className="flex flex-row basis-4/12 space-x-3 text-gray font-poppins text-xs font-bold">
+            <button>Home</button>
+            <div className="flex">
+              <button>Shop</button>
+              <span class="material-symbols-outlined">expand_more</span>
+            </div>
+            <button>About</button>
+            <button>Contact</button>
+            <button>Pages</button>
+          </div>
+          <div className="flex flex-row basis-5/12 space-x-3 justify-end text-gray font-poppins text-xs font-light">
+            <span class="material-symbols-outlined">search</span>
+            <span class="material-symbols-outlined">shopping_cart</span>
+            <span class="material-symbols-outlined">favorite</span>
+            <div className="flex flex-row text-secblue font-bold">
+              <span class="material-symbols-outlined">person</span>
+              <div className="flex pt-0.5">
+                <button className="px-0.2">Login</button>/
+                <button className="px-0.2 ">Register</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   );
-}
+};
 
 export default NavBar;
