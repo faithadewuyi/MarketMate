e410936a797970b229fc0865becdda8d5ce58e59
Elizabeth <betty4web@gmail.com>
2024-04-01 21:16:06 +0100

    Input component

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function InputField() {
-  return <div>InputField</div>;
-}
+const InputField = ({
+  label,
+  type,
+  id,
+  name,
+  register,
+  error,
+  pattern,
+  patternMessage,
+}) => (
+  <div>
+    <label htmlFor={id}>{label}</label>
+    <div className={`${error ? "error-input" : ""}`}>
+      <input
+        type={type || "text"}
+        id={id}
+        {...register(name, {
+          required: `${id} is required`,
+          pattern: {
+            value: pattern,
+            message: patternMessage || "Invalid input format",
+          },
+        })}
+      />
+    </div>
+    <p className="error">{error?.message}</p>
+  </div>
+);
+
+InputField.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  register: PropTypes.func.isRequired,
+  error: PropTypes.object,
+  pattern: PropTypes.instanceOf(RegExp),
+  patternMessage: PropTypes.string,
+  id: PropTypes.string.isRequired,
+};
 
 export default InputField;
