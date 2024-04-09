a11a75715c50196d82079ebc689f58f99f0a57ac
faithadewuyi <iyanuleke09@gmail.com>
2024-04-09 13:56:24 +0100

    Updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,13 +6,13 @@ const About = () => {
     <div className="w-screen">
       <NavBar />
      <div className="min-h-screen"> 
-     <div className="  w-[1440px] h-[8388px] ">
+     <div className="  ">
       <div className="mx-auto text-center space-y-16 bg-[#0E2744] w-full">
       
       <p className="text-[40px] font-catamaran text-black">Discover, Connect and Thirve in Our Marketplace</p>
       <h1 className="font-catamaran font-bold text-[52px] text-white">Find the Perfect Service</h1>
-      <p className="text-[40px] font-catamaran text-white">Discover, Connect and Thirve in Our Marketplace</p>
-      <button>Learn More</button>
+      <p className="text-[25px] font-catamaran text-white mt-0">Discover, Connect and Thrive in Our Marketplace</p>
+      <button className="border-none">Learn More</button>
       <button>Get Started</button>
       </div> 
       </div>
